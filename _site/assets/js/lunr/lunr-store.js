0d710cb8523204e5ccdf6093504e227fbf350a33
koes1124 <koes1124@iabacus.co.kr>
2023-02-06 15:01:25 +0900

    [post]

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Blog","git"],
         "url": "/firstPost/",
         "teaser": null
+      },{
+        "title": "앞으로 매일 1가지의 포스팅을 하도록 노력해봅시다.",
+        "excerpt":"벌써 입사 1년 조금 지났다. 그런데 얻은 것 보다 잃은게 더 많은 느낌이다.  ","categories": [],
+        "tags": ["Blog","git"],
+        "url": "/test/",
+        "teaser": null
       }]
