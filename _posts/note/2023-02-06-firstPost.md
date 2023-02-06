a0647f5a45846739fa287c384b9b9d4c21a161c3
koes1124 <koes1124@iabacus.co.kr>
2023-02-06 14:36:17 +0900

    [post] 첫 포스팅

---
+++ _posts/note/2023-02-06-firstPost.md
@@ -0,0 +1,12 @@
+---
+title: "github.io 블로그 첫 포스팅입니다"
+excerpt: "Github Blog글을 처음 써보며."
+
+categorise:
+  - Blog
+tags:
+  - Blog
+  - git
+---
+
+포스팅 내용 입니다.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io 블로그 첫 포스팅입니다",
+        "excerpt":"포스팅 내용 입니다.  ","categories": [],
+        "tags": ["Blog","git"],
+        "url": "/firstPost/",
+        "teaser": null
+      }]
